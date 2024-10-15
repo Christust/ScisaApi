be33810d06485e7a0b4981676c716e7673b4a588
Christos Marroquin <christos.marroquin@hotmail.com>
2024-10-15 17:12:52 -0600

    Feature: Add Retrieves DTOs for Categories and Products

---
+++ ScisaApi/DTOs/RetireveCategory.cs
@@ -0,0 +1,12 @@
+﻿using ScisaApi.Models;
+
+namespace ScisaApi.DTOs
+{
+    public class RetrieveCategory
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public ICollection<Product> Products { get; set; } = new List<Product>();
+    }
+}
---
+++ ScisaApi/DTOs/RetrieveProduct.cs
@@ -0,0 +1,13 @@
+﻿using ScisaApi.Models;
+
+namespace ScisaApi.DTOs
+{
+    public class RetrieveProduct
+    {
+
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public ICollection<Category> Categories { get; set; } = new List<Category>();
+    }
+}
